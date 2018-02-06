b724531898f00f649139bb0967a7d362c8f62416
Murilo Fujita <murilofujita@fujita.com>
2018-02-06 17:35:04 -0200

    Cena da floresta.

---
+++ Assets/Scripts/Cenario.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cenario : MonoBehaviour 
+{
+	
+	void Start () 
+    {
+		
+	}	
+	
+	void Update () 
+    {
+        transform.Translate(Vector3.left * .05f);
+        if (transform.position.x < -20f)
+        {
+            transform.position = new Vector3(18.7f, transform.position.y, transform.position.z);
+        }
+	}
+}
