6d6e04e22fe2e2f83d36d57e250c6db96e7a6540
MURILO FUJITA <murilo.fujita@fatec.sp.gov.br>
2018-02-26 13:09:24 -0300

    Ajuste de velocidade dos inimigos.

---
+++ Assets/Scripts/InimigoMovimento.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InimigoMovimento : MonoBehaviour 
+{
+    public static bool[] liberaInimigos = new bool[4];
+    private int indice = 0;
+
+    void Start()
+    {
+//        for (int i = 0; i < liberaInimigos.Length; i++)
+//        {
+//            liberaInimigos[i] = false;
+//        }
+    }
+
+	void Update () 
+    {
+        transform.Translate(Vector3.left * 0.1f);
+        if (transform.position.x < -15f)
+        {
+            Destroy(gameObject);
+            liberaInimigos[indice] = true;
+            indice++;
+
+        }
+//        Debug.Log(indice);
+	}
+}
--- Assets/Scripts/PlataformaInimigos.cs
+++ Assets/Scripts/PlataformaInimigos.cs
@@ -6,13 +6,35 @@ public class PlataformaInimigos : MonoBehaviour
 {
     public Transform plataforma;
     public Transform rato;
+    public Transform bruxa;
 
 	void Start () 
     {
-        Instantiate(plataforma, new Vector3(15.7f, -1.5f, 0), Quaternion.identity);
+        Instantiate(plataforma, new Vector3(15.7f, -1.3f, 0), Quaternion.identity);
         Instantiate(rato, new Vector3(22f, -3.82f, 0), Quaternion.identity);
 
-        Instantiate(plataforma, new Vector3(21.7f, 0f, 0), Quaternion.identity);
-        Instantiate(rato, new Vector3(30f, -3.82f, 0), Quaternion.identity);
+
 	}
+
+    void Update()
+    {
+        if (InimigoMovimento.liberaInimigos[0])
+        {
+            Instantiate(plataforma, new Vector3(15.7f, -1.3f, 0), Quaternion.identity);
+            Instantiate(rato, new Vector3(22f, -3.82f, 0), Quaternion.identity);
+        }
+
+        if (InimigoMovimento.liberaInimigos[1])
+        {
+            Instantiate(plataforma, new Vector3(14f, 0f, 0), Quaternion.identity);
+            Instantiate(bruxa, new Vector3(22f, 3.5f, 0), Quaternion.identity);
+        }
+
+        if (InimigoMovimento.liberaInimigos[2])
+        {
+            Instantiate(plataforma, new Vector3(28.7f, 0f, 0), Quaternion.identity);
+            Instantiate(bruxa, new Vector3(32f, 3.55f, 0), Quaternion.identity);
+        }
+        Debug.Log("Indices: " + InimigoMovimento.liberaInimigos[0] + " " + InimigoMovimento.liberaInimigos[1] + " " + InimigoMovimento.liberaInimigos[2]);
+    }
 }
