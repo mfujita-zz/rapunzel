01fd9547b250c9540476de693b38170e98269090
MURILO FUJITA <murilo.fujita@fatec.sp.gov.br>
2018-03-05 00:10:24 -0300

    Ajuste de fases.

---
+++ Assets/Scripts/FlorestaBotao.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FlorestaBotao : MonoBehaviour 
+{
+//    Scene nomeCena;
+//
+//    void Awake ()
+//    {
+//        nomeCena = SceneManager.GetActiveScene();
+//    }
+
+    public void ReiniciarFloresta()
+    {        
+//        SceneManager.LoadScene(nomeCena.ToString(), LoadSceneMode.Single);
+        SceneManager.LoadScene("Transicao2_3");
+    }
+}
